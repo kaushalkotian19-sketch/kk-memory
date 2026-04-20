79c0ccc236d34cfa53bd4a2feeeb1729d5004e7c
kaushalkotian19-sketch <kaushalkotian19@gmail.com>
2026-04-20 17:47:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,37 @@
-self.addEventListener('install', e=>{
-self.skipWaiting();
+const CACHE_NAME = 'memories-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/icon.jpg',
+  '/manifest.json'
+];
+
+// Install: Cache core assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate: Cleanup old caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch: Serve from cache, fallback to network
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
